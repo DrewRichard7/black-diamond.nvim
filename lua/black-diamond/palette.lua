ea2e754eb600f734d20fe73229a88a5940994b7e
Andrew Richard <43729071+DrewRichard7@users.noreply.github.com>
2025-11-20 15:29:31 -0800

    had some hex+alphas

--- lua/black-diamond/palette.lua
+++ lua/black-diamond/palette.lua
@@ -2,27 +2,32 @@ local M = {}
 
 M.colors = {
 	bg = "#161d26", -- editor.background
-	bg_dark = "#12181f", -- activityBar.background (for sidebars)
-	bg_float = "#1f2936", -- editorWidget.background
-	bg_visual = "#324256", -- list.activeSelectionBackground (approx)
-	bg_highlight = "#11b7d415", -- gentle background highlight
+	bg_dark = "#12181f", -- sidebars
+	bg_float = "#1f2936", -- widgets/floating windows
 
-	fg = "#c5cfdc", -- editor.foreground
-	fg_dark = "#a5b5c9", -- generic foreground
+	-- FIXED: These are now solid colors representing the blend
+	-- Originally #11b7d415 (Cyan @ 8% on BG)
+	bg_highlight = "#1b252f",
+
+	-- Originally #324256 (Visual Selection)
+	bg_visual = "#324256",
+
+	fg = "#c5cfdc", -- default text
+	fg_dark = "#a5b5c9", -- darker text
 	fg_comment = "#465b76", -- comments
-	fg_gutter = "#394352", -- editorLineNumber.foreground
+	fg_gutter = "#394352", -- line numbers
 
-	cyan = "#11B7D4", -- primary accent
-	green = "#00a884", -- strings
-	orange = "#d4770c", -- numbers/properties
-	yellow = "#c7910c", -- keywords
-	red = "#E35535", -- errors/constants
-	purple = "#d46ec0", -- parameters
-	violet = "#a85ff1", -- classes/types
-	pink = "#c62f52", -- variables
-	blue_dark = "#3585bb", -- entity types
+	cyan = "#11B7D4",
+	green = "#00a884",
+	orange = "#d4770c",
+	yellow = "#c7910c",
+	red = "#E35535",
+	purple = "#d46ec0",
+	violet = "#a85ff1",
+	pink = "#c62f52",
+	blue_dark = "#3585bb",
 
-	border = "#0b0e13", -- borders
+	border = "#0b0e13",
 }
 
 return M
